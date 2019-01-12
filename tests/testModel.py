4ebb5e82ffe2ef8d4933aa9ceedf44d1ca4273ce
Will Xia <gicalc32@gmail.com>
2019-01-12 17:02:19 -0500

    New Files

--- tests/testModel.py
+++ tests/testModel.py
@@ -3,12 +3,26 @@ import numpy as np
 import os,glob,cv2
 import sys,argparse
 
-images = []
-image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
-images.append(image)
-images = np.array(images, dtype=np.uint8)
-images = images.astype('float32')
-images = np.multiply(images, 1.0/255.0)
+#Data directory
+dat_dir = '../data'
+test_dir = dat_dir + '/test'
+
+# Load Images
+def preprocess(im):
+    images = []
+    image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
+    images.append(image)
+    images = np.array(images, dtype=np.uint8)
+    images = images.astype('float32')
+    images = np.multiply(images, 1.0/255.0)
+    return images
+
+classes = os.listdir(train_path)
+#Remove .DS_Store
+for i,j in enumerate(classes):
+    if j == '.DS_Store':
+        del classes[i]
+        break
 
 x_batch = images.reshape(1, image_size,image_size,num_channels)
 
@@ -21,3 +35,5 @@ y_pred = graph.get_tensor_by_name('y_pred:0')
 x = graph.get_tensor_by_name('x:0')
 y_true = graph.get_tensor_by_name('y_true:0')
 y_test_images = np.zeros((1, len(labels)))
+feed_dict_testing = {x: x_batch, y_true: y_test_images}
+result=sess.run(y_pred, feed_dict=feed_dict_testing)
